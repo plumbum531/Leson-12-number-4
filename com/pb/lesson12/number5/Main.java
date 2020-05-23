45b70d06496127d31032ca20489528d7d0bdb352
Volodymyr_Shabalin <Volodymyr.Shabalin@nordstrom.com>
2020-05-23 22:12:12 +0300

    fix, add exclamation mark in hello message

--- com/pb/lesson12/number5/Main.java
+++ com/pb/lesson12/number5/Main.java
@@ -2,6 +2,6 @@ package com.pb.lesson12.number5;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HELLO WORLD");
+        System.out.println("HELLO WORLD!");
     }
 }
